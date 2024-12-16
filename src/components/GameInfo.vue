086df3a0b2f2788d0af120a50ea6091d00e7e4bc
pniebrzydowski <patrick.niebrzydowski@gmail.com>
2024-12-16 10:53:27 +0100

    wk3-exercise: add name input fields/difficulty selector & display on play screen (#7)

---
+++ src/components/GameInfo.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useGameStore } from '@/store/game'
+
+const gameStore = useGameStore()
+</script>
+
+<template>
+  <p>Difficulty: {{ gameStore.difficultyLabel }}</p>
+</template>
+
+<style scoped></style>
---
+++ src/components/UserInfo.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useUserStore } from '@/store/user'
+
+const userStore = useUserStore()
+</script>
+
+<template>
+  <p>{{ userStore.fullName }}</p>
+</template>
+
+<style scoped></style>
