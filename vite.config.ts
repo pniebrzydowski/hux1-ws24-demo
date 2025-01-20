f8a91112ffa461c01aba673dda36b59c916218c3
Patrick Niebrzydowski <patrick.niebrzydowski@gmail.com>
2025-01-20 12:04:14 +0100

    wk6-demo: add basepath

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/hux1-ws24-demo/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
